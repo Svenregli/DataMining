fe31bc469fefa0f1fadbde90188e52bbfe586aa1
Sven Regli <sven.regli@ad.asatr.ch>
2025-02-24 19:10:34 +0100

    #added Arxiv API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import arxiv
+import pandas as pd
+import re
 
 search = arxiv.Search(
     query='cat:cs.CL',
@@ -8,18 +10,21 @@ search = arxiv.Search(
 
 client = arxiv.Client()
 papers = list(client.results(search))
-print(papers[1])
 
-{
-    'authors': papers[1].authors,
-    'categories':papers[1].categories,
-    'entry_id': papers[1].entry_id,
-    'pdf_url': papers[1].pdf_url,
-    'primary_catgeory': papers[1].primary_category,
-    'summary': papers[1].summary,
-    'title': papers[1].title,
-    'published': papers[1].published,
-    'doi': papers[1].doi
+dataset = []
+for result in papers: 
+    result.pdf_url = re.sub(r'v\d+$', '', result.pdf_url)
+    dataset.append({
+        'authors': result.authors,
+        'categories': result.categories,
+        'entry_id': result.entry_id,
+        'pdf_url': result.pdf_url,
+        'primary_category': result.primary_category,  # corrected key spelling
+        'summary': result.summary,
+        'title': result.title,
+        'published': result.published,
+        'doi': result.doi   
+    })
 
-    
-}
+dataset = pd.DataFrame(dataset)
+print(dataset.head())
